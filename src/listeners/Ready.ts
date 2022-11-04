6aac2ba8dfb132d8fe200e21a14edc4e63d3c247
bitomic <35199700+bitomic@users.noreply.github.com>
2022-11-03 23:41:19 -0600

    feat: set presence

--- src/listeners/Ready.ts
+++ src/listeners/Ready.ts
@@ -11,6 +11,11 @@ export class UserEvent extends Listener {
 		this.container.logger.info( `Ready! as ${ this.container.client.user?.tag ?? 'unknown user' }` )
 
 		await this.container.sequelize.sync()
-		console.log( process.env.npm_package_version )
+		this.container.client.user?.setPresence( {
+			activities: [ {
+				name: `Pokémon | Versión ${ process.env.npm_package_version ?? '1.0.0' }`,
+				type: 'COMPETING'
+			} ]
+		} )
 	}
 }
