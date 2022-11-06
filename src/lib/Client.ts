3a50fba11eed6294cf210d961e0fa589bc5eaaa3
bitomic <35199700+bitomic@users.noreply.github.com>
2022-11-06 16:17:58 -0700

    feat: schedule a task to close battles and trades (#40)
    * chore(deps): add @sapphire/plugin-scheduled-tasks and bullmq
    
    * feat: setup scheduled tasks
    
    * feat: schedule a task to close battles and trades
    
    * chore: set version to 1.1.3

--- src/lib/Client.ts
+++ src/lib/Client.ts
@@ -3,6 +3,7 @@ import { env } from './environment'
 import { Intents } from 'discord.js'
 import { ModelStore } from '../framework'
 import Redis from 'ioredis'
+import { ScheduledTaskRedisStrategy } from '@sapphire/plugin-scheduled-tasks/register-redis'
 import type { Sequelize } from 'sequelize'
 import { sequelize } from './Sequelize'
 
@@ -20,7 +21,20 @@ export class UserClient extends SapphireClient {
 			logger: {
 				level: LogLevel.Info
 			},
-			partials: [ 'CHANNEL', 'MESSAGE', 'REACTION' ]
+			partials: [ 'CHANNEL', 'MESSAGE', 'REACTION' ],
+			tasks: {
+				strategy: new ScheduledTaskRedisStrategy( {
+					bull: {
+						connection: {
+							db: env.REDIS_DB,
+							host: env.REDIS_HOST,
+							password: env.REDIS_PASSWORD,
+							port: env.REDIS_PORT,
+							username: env.REDIS_USERNAME
+						}
+					}
+				} )
+			}
 		} )
 		container.redis = new Redis( {
 			db: env.REDIS_DB,
---
+++ src/scheduled-tasks/CloseBattleTrade.ts
@@ -0,0 +1,59 @@
+import { ScheduledTask, type ScheduledTaskOptions } from '@sapphire/plugin-scheduled-tasks'
+import { ApplyOptions } from '@sapphire/decorators'
+import Colors from '@bitomic/material-colors'
+
+@ApplyOptions<ScheduledTaskOptions>( {
+	name: 'close-battle-trade'
+} )
+export class UserTask extends ScheduledTask {
+	public async run( payload: { channelId?: string, force?: boolean, messageId?: string, type?: string } ): Promise<void> {
+		const { channelId, force = false, messageId, type } = payload
+		if ( !channelId || !messageId || !type ) {
+			this.container.logger.warn( 'Tried to run the close-battle-trade task with an incomplete payload.', payload )
+			return
+		}
+
+		const channel = await this.container.client.channels.fetch( channelId )
+			.catch( () => null )
+		if ( !channel || channel.type !== 'GUILD_TEXT' ) return
+
+		const message = await channel.messages.fetch( messageId )
+			.catch( () => null )
+		if ( !message || message.components.length === 0 ) return
+
+		if ( !force && message.thread ) {
+			const lastMessageTime = message.thread.lastMessage?.createdTimestamp
+
+			// if there was a message in the last hour, don't close
+			if ( lastMessageTime && lastMessageTime + 1000 * 60 * 60 > Date.now() ) {
+				await this.container.tasks.create( 'close-battle-trade', payload, 1000 * 60 * 60 )
+				return
+			}
+		}
+
+		await message.edit( {
+			components: [],
+			content: `Este ${ type } ya no está disponible.`,
+			embeds: message.embeds.map( i => {
+				i.color = Colors.amber.s800
+				return i
+			} )
+		} )
+
+		if ( message.thread ) {
+			await message.thread.send( {
+				embeds: [ {
+					color: Colors.teal.s800,
+					description: `Este ${ type } ha sido cerrado automáticamente tras una hora de inactividad.`
+				} ]
+			} )
+			await message.thread.edit( { archived: true, locked: true } )
+		}
+	}
+}
+
+declare module '@sapphire/plugin-scheduled-tasks' {
+	interface ScheduledTasks {
+		'close-battle-trade': never
+	}
+}
