12a289261b792cbf5fa004193538e355b75cfeca
bitomic <35199700+bitomic@users.noreply.github.com>
2022-11-06 17:51:35 -0700

    feat: task analytics

---
+++ src/listeners/analytics/TaskFinished.ts
@@ -0,0 +1,13 @@
+import { Listener, type ListenerOptions } from '@sapphire/framework'
+import { ApplyOptions } from '@sapphire/decorators'
+import { ScheduledTaskEvents } from '@sapphire/plugin-scheduled-tasks'
+
+@ApplyOptions<ListenerOptions>( {
+	event: ScheduledTaskEvents.ScheduledTaskRun
+} )
+export class UserEvent extends Listener {
+	public run( task: string, payload: unknown ): void {
+		void this.container.stores.get( 'models' ).get( 'task-analytics' )
+			.register( task, 'finished', payload )
+	}
+}
