499fe4370f95310e05cc62479fbbe6f3a1361a71
bitomic <35199700+bitomic@users.noreply.github.com>
2022-11-09 14:06:50 -0700

    fix: get general channel from db (#56)
    * fix: set general channel
    
    * fix: get general channel from database, and only if it's in the same guild
    
    * chore: set version to 1.3.4

--- src/listeners/MemberUpdate.ts
+++ src/listeners/MemberUpdate.ts
@@ -1,6 +1,7 @@
 import { Constants, type GuildMember } from 'discord.js'
 import { Listener, type ListenerOptions } from '@sapphire/framework'
 import { ApplyOptions } from '@sapphire/decorators'
+import { ChannelTypes } from '../utils'
 
 @ApplyOptions<ListenerOptions>( {
 	event: Constants.Events.GUILD_MEMBER_UPDATE
@@ -8,9 +9,13 @@ import { ApplyOptions } from '@sapphire/decorators'
 export class UserEvent extends Listener {
 	public async run( oldMember: GuildMember, newMember: GuildMember ): Promise<void> {
 		if ( oldMember.roles.cache.size === 1 && newMember.roles.cache.size > 1 ) {
-			const channel = await this.container.client.channels.fetch( '1038642069528399884' )
+			const generalId = await this.container.stores.get( 'models' ).get( 'channel-settings' )
+				.getSettingCache( 'type', ChannelTypes.GENERAL )
+			if ( !generalId ) return
+
+			const channel = await this.container.client.channels.fetch( generalId )
 				.catch( () => null )
-			if ( !channel || channel.type !== 'GUILD_TEXT' ) return
+			if ( !channel || channel.type !== 'GUILD_TEXT' || oldMember.guild.id !== channel.guildId ) return
 			await channel.send( `¡Hola, <@!${ oldMember.user.id }>!` )
 		}
 	}
