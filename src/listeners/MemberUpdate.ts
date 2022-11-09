78b3c3eb2f7e4d72c57070325f524132326767e2
bitomic <35199700+bitomic@users.noreply.github.com>
2022-11-09 13:09:55 -0600

    fix: prevent unintended greetings

--- src/listeners/MemberUpdate.ts
+++ src/listeners/MemberUpdate.ts
@@ -6,11 +6,12 @@ import { ApplyOptions } from '@sapphire/decorators'
 	event: Constants.Events.GUILD_MEMBER_UPDATE
 } )
 export class UserEvent extends Listener {
-	public async run( oldMember: GuildMember ): Promise<void> {
-		if ( oldMember.roles.cache.size !== 0 ) return
-		const channel = await this.container.client.channels.fetch( '1038642069528399884' )
-			.catch( () => null )
-		if ( !channel || channel.type !== 'GUILD_TEXT' ) return
-		await channel.send( `¡Hola, <@!${ oldMember.user.id }>!` )
+	public async run( oldMember: GuildMember, newMember: GuildMember ): Promise<void> {
+		if ( oldMember.roles.cache.size === 0 && newMember.roles.cache.size > 0 ) {
+			const channel = await this.container.client.channels.fetch( '1038642069528399884' )
+				.catch( () => null )
+			if ( !channel || channel.type !== 'GUILD_TEXT' ) return
+			await channel.send( `¡Hola, <@!${ oldMember.user.id }>!` )
+		}
 	}
 }
